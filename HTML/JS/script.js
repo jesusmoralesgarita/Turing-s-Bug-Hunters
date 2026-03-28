95dc177b40fef6dfd8f82b197381d7be281d4797
Leilani Sofia López Rivera <leilanilopezr96@gmail.com>
2026-03-27 23:09:06 -0600

    Cambios en footer, links y fuentes

--- HTML/JS/script.js
+++ HTML/JS/script.js
@@ -14,4 +14,11 @@
 
 //Luis
 
-//Leila
+//Leila - Footer
+
+// Cambio de año automático en Copyright
+const cambioAnio = document.querySelector("footer p.text-center");
+const anioActual = new Date().getFullYear();
+cambioAnio.textContent = `Copyright © ${anioActual} Turing's Bug Hunters S.A de C.V. All Rights Reserved.`;
+
+
