9497b14fd5b037c9657676ce42ce780e6c273e3d
gioTlahuiz <fcei.tlahuiz.2.e@gmail.com>
2026-04-06 13:43:08 -0600

    boton de enviar y funciones para el modal con mensaje

--- HTML/JS/contacto.js
+++ HTML/JS/contacto.js
@@ -5,8 +5,14 @@
 
 
     /* Gio  */
+    // función para el boton de enviar de la pagina contacto
+    function enviarFormulario(){
+        document.getElementById("modal").style.display = "block";
+    }
 
-
+    function cerrarModal(){
+        document.getElementById("modal").style.display = "none";
+    }
     /* Erick */
 
 
