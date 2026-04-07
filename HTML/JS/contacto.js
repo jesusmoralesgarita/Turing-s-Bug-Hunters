1bbc334b472488ac92d265658f82e6888b552449
EMGA <erickmgarciaa@outlook.com>
2026-04-07 17:50:30 -0600

    Funciones de validacion echas

--- HTML/JS/contacto.js
+++ HTML/JS/contacto.js
@@ -1,31 +1,40 @@
-    /* Oswaldo */
+/* Oswaldo */
 
 
-    /* Luis */
+/* Luis */
 (() => {
-  'use strict'
+    'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
 
-        const divs = form.getElementsByClassName("form-element");
+            const divs = form.getElementsByClassName("form-element");
 
             Array.from(divs).forEach(div => {
                 let message = "";
                 const divInput = div.getElementsByTagName("input").item(0);
 
-                    console.log(divInput)
-                    const inputType = divInput.getAttribute("valtype");
+                console.log(divInput)
+                const inputType = divInput.getAttribute("valtype");
 
 
-               if(inputType !== null){
-                    switch(inputType){
-                        case "email":
+                if (inputType !== null) {
+                    switch (inputType) {
+                        case "nombrehomes":
                             message = validarNombre();
+                            break;
+                        case "number":
+                            message = validarNumCel();
+                            break;
+                        case "email":
+                            message = validarEmail1();
+                            break;
+                        case "mensaje":
+                            message = validarMensaje();
                             break;
                         default:
                             console.log(divInput.getAttribute("valtype"))
@@ -38,26 +47,26 @@
                         divInput.setCustomValidity(message);
                     }
                 }
-                
-                
+
+
             });
 
             if (!form.checkValidity()) {
                 event.preventDefault()
                 event.stopPropagation()
             }
-        form.classList.add('was-validated')
-           
+            form.classList.add('was-validated')
 
-    }, false)
-  })
+
+        }, false)
+    })
 })()
 
 
-    /* Gio  */
+/* Gio  */
 
 
-    /* Erick */
+/* Erick */
 // Función para validar email con expresión regular
 function validarEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -68,70 +77,80 @@ function validarEmail(email) {
 document.getElementById("registroForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Evita el envío automático */
 
-    // Limpiar mensajes previos
-    //document.querySelectorAll(".error").forEach(e => e.textContent = "");
+// Limpiar mensajes previos
+//document.querySelectorAll(".error").forEach(e => e.textContent = "");
 
-    
 
-    // Validar nombre
-    function validarNombre(){
 
-        
-        const nombre = document.getElementById("contact-name").value.trim();
-        if (nombre === "") {
-        
+// Validar nombre
+function validarNombre() {
+
+
+    const nombre = document.getElementById("contact-name").value.trim();
+    if (nombre === "") {
         return "El nombre es obligatorio.";
-        }
-        if(!/\d/.test(nombre)){
-        return "OK"
-        }else{
+    }
+    if ((!/\d/.test(nombre))) {
+        return "";
+    } else {
         return "El nombre sin números";
-
-        }
-    };
-    
-    // Validar email
-    function validarEmail(){
-
-        const email = document.getElementById("email").value.trim();
-        if (email === "") {
-        document.getElementById("errorEmail").textContent = "El email es obligatorio.";
-        valido = false;
-        } else if (!validarEmail(email)) {
-        document.getElementById("errorEmail").textContent = "Formato de email inválido.";
-        valido = false;
-        }
-    };
-    
-     // Validar numero de telefono
-    const cel = document.getElementById("cel").value;
-    if (typeof cel !== 'number') {
-        document.getElementById("errorCel").textContent = "Solo se aceptan numeros";
-        valido = false;
     }
-    if (cel.length < 10){
-        document.getElementById("errorCel").textContent = "La numero debe tener al menos 10 digitos.";
-        valido = false;
+};
+
+// Validar email
+function validarEmail1() {
+
+    const email = document.getElementById("contact-email").value.trim();
+    if (email === "") {
+        return "El email es obligatorio.";
+    }
+    if (!validarEmail(email)) {
+        return "Formato de email inválido.";
+    } else {
+        return "";
     }
 
-    // Validar contraseña
-    const password = document.getElementById("password").value;
-    if (password.length < 6) {
-        document.getElementById("errorPassword").textContent = "La contraseña debe tener al menos 6 caracteres.";
-        valido = false;
+};
+
+// Validar numero de telefono
+function validarNumCel() {
+
+    const cel = document.getElementById("contact-number").value.trim();
+    if (/^\d$/.test(cel)) {
+        return "Solo se aceptan números";
+    }
+    if (cel.length == 10) {
+        return "El número de telefono debe tener al menos 10 digitos.";
+    } else {
+        return "";
     }
 
-    // Si todo es válido, enviar formulario
-    if (valido) {
-        alert("Registro realizado con exito");
-        this.submit();
+}
+
+
+
+// Validar mensaje
+function validarMensaje() {
+    const mensaje = document.getElementById("contact-menssage").value.trim();
+    if (mensaje.length < 0 && mensaje.length <= 250) {
+        return "";
+    }else{
+        return "Coloca un mensaje"
     }
+
+}
+
+
+// Si todo es válido, enviar formulario
+if (valido) {
+    alert("Registro realizado con exito");
+    this.submit();
+}
 //});
 
-    /* Mai */
+/* Mai */
 
 
-    /* Alex */
+/* Alex */
 
 
-    
