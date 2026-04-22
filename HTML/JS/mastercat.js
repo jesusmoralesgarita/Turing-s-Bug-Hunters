e14b314631c2ce349ba596c9fc293cbc424335bc
Oswaldo <santoswaldos@gmail.com>
2026-04-21 21:50:50 -0600

    Agregando botón de eliminar de forma modal

--- HTML/JS/mastercat.js
+++ HTML/JS/mastercat.js
@@ -27,7 +27,13 @@
 
 
 /* OSWALDO */
+function mostrarAlerta() {
+  document.getElementById("miAlerta").style.display = "block";
+}
 
+function cerrarAlerta() {
+  document.getElementById("miAlerta").style.display = "none";
+}
 
 
 /* ALE */
