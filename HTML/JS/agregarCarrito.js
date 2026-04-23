b8da51a67b860927651111ad53d8af91f6978043
EMGA <erickmgarciaa@outlook.com>
2026-04-22 20:14:37 -0600

    previsualisacion de imagen ingresada

--- HTML/JS/agregarCarrito.js
+++ HTML/JS/agregarCarrito.js
@@ -1,6 +1,23 @@
 //  Erick
+/*
+const inputImagen = document.getElementById("inputImagen");
+const preview = document.getElementById("preview");
+const guardarBtn = document.getElementById("guardarBtn");
 
 
+inputImagen.addEventListener("change", function(event) {
+  const archivo = event.target.files[0];
+  if (archivo) {
+    const imagenURL = URL.createObjectURL(archivo);
+    preview.src = imagenURL;
+    preview.classList.remove("d-none");
+  }
+});
+
+guardarBtn.addEventListener("click", function() {
+  inputImagen.click();
+});
+*/
 
 // Fer
 
