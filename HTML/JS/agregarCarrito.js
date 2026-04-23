6ac0f7c34c50cb0c9eb58e23433dda6917ec590d
EMGA <erickmgarciaa@outlook.com>
2026-04-23 16:28:28 -0600

    parte alejandro y erick juntas

--- HTML/JS/agregarCarrito.js
+++ HTML/JS/agregarCarrito.js
@@ -1,9 +1,6 @@
 //  Erick
-/*
-const inputImagen = document.getElementById("inputImagen");
+const inputImagen = document.getElementById('file-input-ale');
 const preview = document.getElementById("preview");
-const guardarBtn = document.getElementById("guardarBtn");
-
 
 inputImagen.addEventListener("change", function(event) {
   const archivo = event.target.files[0];
@@ -13,6 +10,13 @@ inputImagen.addEventListener("change", function(event) {
     preview.classList.remove("d-none");
   }
 });
+/*
+
+
+const guardarBtn = document.getElementById("guardarBtn");
+
+
+
 
 guardarBtn.addEventListener("click", function() {
   inputImagen.click();
@@ -24,3 +28,18 @@ guardarBtn.addEventListener("click", function() {
 
 
 // Ale
+const dropZoneAle = document.getElementById('drop-zone-ale');
+const fileInputAle = document.getElementById('file-input-ale');
+
+dropZoneAle.addEventListener('click', () => fileInputAle.click());
+
+fileInputAle.addEventListener('change', function() {
+    if (this.files && this.files[0]) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const preview = document.querySelector('.img-preview-ale');
+            preview.src = e.target.result;
+        };
+        reader.readAsDataURL(this.files[0]);
+    }
+});
