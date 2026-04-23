418d2bc0b4cd09bcbd028775c18f5fc8aa47a05f
jafethalejandroruizrocha-source <jafethalejandroruizrocha@gmail.com>
2026-04-22 20:14:08 -0600

    agregar carrito

--- HTML/JS/agregarCarrito.js
+++ HTML/JS/agregarCarrito.js
@@ -7,3 +7,18 @@
 
 
 // Ale
+const dropZoneAle = document.getElementById('drop-zone-ale');
+const fileInputAle = document.getElementById('file-input-ale');
+
+dropZoneAle.addEventListener('click', () => fileInputAle.click());
+
+fileInputAle.addEventListener('change', function() {
+    if (this.files && this.files[0]) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const preview = document.querySelector('.img-preview-ale');
+            preview.src = e.target.result;
+        };
+        reader.readAsDataURL(this.files[0]);
+    }
+});
